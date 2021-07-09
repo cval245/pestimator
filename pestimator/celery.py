70cad7afe24df2a8c83195c608f8b5a951351513
Chris <c.val@tutanota.com>
2021-07-09 11:11:21 -0500

    rabbit

--- pestimator/celery.py
+++ pestimator/celery.py
@@ -1,16 +1,48 @@
-import os
+# import os
 
-from celery import Celery
+# from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pestimator.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pestimator.settings')
 
-app = Celery('pestimator')
+# app = Celery('pestimator')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
-app.conf.update(BROKER_URL=os.environ['amqps://yivfkhqd:WrcWUxS7CP45fshvaNkIhEzEGLJyP6_2@fish.rmq.cloudamqp.com/yivfkhqd'])
+# app.autodiscover_tasks()
+# app.conf.update(BROKER_URL=os.environ['amqps://yivfkhqd:WrcWUxS7CP45fshvaNkIhEzEGLJyP6_2@fish.rmq.cloudamqp.com/yivfkhqd'])
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
+
+
+
+import pika, os, signal, sys
+
+def signal_handler(signal, frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL', 'amqps://yivfkhqd:WrcWUxS7CP45fshvaNkIhEzEGLJyP6_2@fish.rmq.cloudamqp.com/yivfkhqd')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello') # Declare a queue
+channel.basic_publish(exchange='',
+                  routing_key='hello',
+                  body='Hello CloudAMQP!')
+
+print(" [x] Sent 'Hello World!'")
+
+def callback(ch, method, properties, body):
+  print(" [x] Received " + str(body))
+
+channel.basic_consume('hello',
+                      callback,
+                      auto_ack=True)
+
+print(' [*] Waiting for messages:')
+channel.start_consuming()
+connection.close()
