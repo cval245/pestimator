74ccad6ae782b9661ec6e84486865dd687128056
Chris <c.val@tutanota.com>
2021-12-15 15:32:13 -0600

    manually fixing migrations

--- account/migrations/0002_initial.py
+++ account/migrations/0002_initial.py
@@ -6,24 +6,27 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('characteristics', '0001_initial'),
-        ('lawfirm', '0001_initial'),
+        ('account', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='lawfirm',
+            model_name='userprofile',
+            name='user',
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='purchaseorder',
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
-            model_name='defaultlawfirm',
-            name='country',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='characteristics.country'),
+            model_name='lineitem',
+            name='purchase_order',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='account.purchaseorder'),
         ),
     ]
--- characteristics/tests.py
+++ characteristics/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from characteristics.factories import ApplTypeFactory, CountryFactory
+from user.factories import UserFactory
+
+
+class CharacteristicsTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.applType_prov = ApplTypeFactory(prov=True)
+        self.applType_pct = ApplTypeFactory(pct=True)
+        self.applType_utility = ApplTypeFactory(utility=True)
+        self.country_US = CountryFactory(US=True)
+        self.country_CN = CountryFactory(CN=True)
+        self.country_JP = CountryFactory(country='JP')
+
+    def test_set_up(self):
+        pass
