5bfc10bcdc340056e36a48e5dba7cb5f962bc4c7
Chris <c.val@tutanota.com>
2021-12-24 15:32:40 -0600

    added tests

--- transform/serializers.py
+++ transform/serializers.py
@@ -12,42 +12,42 @@ class TransComplexTimeSerializer(serializers.ModelSerializer):
 class CustomFilingTransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CustomFilingTransform
-		fields = ('id', 'date_diff', 'country', 'appl_type', 'prev_appl_type', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'appl_type', 'prev_appl_type', 'trans_complex_time_condition')
 
 
 class PublicationTransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = PublicationTransform
-		fields = ('id', 'date_diff', 'country', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'trans_complex_time_condition')
 
 
 class RequestExaminationTransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = RequestExaminationTransform
-		fields = ('id', 'date_diff', 'country', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'trans_complex_time_condition')
 
 
 class OATransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = OATransform
-		fields = ('id', 'date_diff', 'country', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'trans_complex_time_condition')
 
 
 class USOATransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = USOATransform
-		fields = ('id', 'date_diff', 'country', 'final_oa_bool', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'final_oa_bool', 'trans_complex_time_condition')
 
 
 class AllowanceTransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = AllowanceTransform
-		fields = ('id', 'date_diff', 'country', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'trans_complex_time_condition')
 
 class IssueTransformSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = IssueTransform
-		fields = ('id', 'date_diff', 'country', 'complex_time_conditions')
+		fields = ('id', 'date_diff', 'country', 'trans_complex_time_condition')
 
 class CountryOANumSerializer(serializers.ModelSerializer):
 	class Meta:
